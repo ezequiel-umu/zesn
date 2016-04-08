076afb61f9b448469f0b4aebab2c5399e4173489
nadie <nadie@nadie.es>
2016-04-08 19:57:55 +0200

    Githook push

--- gitupdate.php
+++ gitupdate.php
@@ -3,6 +3,6 @@ ini_set("log_errors", 1);
 ini_set("error_log", "php-error.log");
 
 // Use in the "Post-Receive URLs" section of your GitHub repo.
-shell_exec( 'cd '. getcwd() . ' && githook-push.sh' );
+shell_exec( 'cd '. getcwd() . ' && ./githook-push.sh' );
 }
 ?>
