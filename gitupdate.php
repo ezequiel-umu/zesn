7b21fdeb5338b29feb84dc954374ecda7329671c
nadie <nadie@nadie.es>
2016-04-08 19:24:26 +0200

    Githook secret.

--- gitupdate.php
+++ gitupdate.php
@@ -1,7 +1,12 @@
 <?php
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 if ( $_POST['payload'] ) {
-  shell_exec( 'cd /var/www/html && git reset --hard HEAD && git pull' );
+  $json = json_decode($_POST['payload']);
+  $remote_secret = $json->{"hook"}->{"secret"};
+  if ($remote_secret === file_get_contents(".github.webhook.secret.key")) {
+    shell_exec( 'cd '. getcwd() . ' && githook-push.sh' );
+  }
+  else
+    echo 'Wrong secret';
 }
-?>hi
-
+?>
