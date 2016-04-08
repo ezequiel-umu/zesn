dff3c9159b1f454482189d0ce34d5fcf991470dd
nadie <nadie@nadie.es>
2016-04-08 19:30:28 +0200

    PHP errors.

--- gitupdate.php
+++ gitupdate.php
@@ -1,6 +1,9 @@
 <?php
+ini_set("log_errors", 1);
+ini_set("error_log", "php-error.log");
+
 // Use in the "Post-Receive URLs" section of your GitHub repo.
-if ( $_POST['payload'] ) {
+if ( isset($_POST['payload']) ) {
   $json = json_decode($_POST['payload']);
   $remote_secret = $json->{"hook"}->{"secret"};
   if ($remote_secret === file_get_contents(".github.webhook.secret.key")) {
